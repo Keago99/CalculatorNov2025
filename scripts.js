af17f9d654c006f6f6b6ad0573e42920ece80c22
Keago99 <keatabde@gmail.com>
2025-11-10 12:58:39 +0200

    Add logic for equalButton and calculate

--- scripts.js
+++ scripts.js
@@ -10,6 +10,29 @@ numberArray = [];
 operatorArray = [];
 historyArray = [];
 
+function equalsEvent(){
+    let screenEqualContent = mainScreen.textContent.trim();
+    if (!screenEqualContent){
+        return;
+    }
+    else{
+        let parsedEqualContent = Number(screenEqualContent);
+        numberArray.push(parsedEqualContent);
+    }
+    if(numberArray.length > 1 && operatorArray.length > 0){
+        calculate();
+    }
+    else{
+        console.log("not enough data, will not work!");
+        return;
+    }
+}
+
+
+function updateSubScreen(){
+    //logic for updating the subScreen
+}
+
 function calculate(){
     let sum = null;
     switch (operatorArray[0]){
@@ -29,24 +52,24 @@ function calculate(){
             sum = numberArray[0] - numberArray[1];
             break;
     }
-    let historyEntry = `${numberArray[0]} ${operatorArray[0]} ${numberArray[1]} = ${sum}`;
+    let historyEntry = `${numberArray[0]} ${operatorArray[0]} ${numberArray[1]} `;
     historyArray.push(historyEntry);
+    console.table(historyArray);
     numberArray.pop();
     numberArray.pop();
     operatorArray.pop();
     mainScreen.textContent = sum;
-    subScreen.textContent = calculationHistory.join(" | ");
+    numberArray.push(sum);
+    console.log(numberArray);
+    console.log(operatorArray);
+
 }
 
 
 function addToMainScreen(event){
     let currentContent = mainScreen.textContent.trim();
-    console.log(currentContent.length);
     if (currentContent.length < 7){
         mainScreen.textContent +=  event.target.textContent;
-    }
-    else{
-        console.log("wrongs");
     }
 }
 
@@ -61,7 +84,15 @@ function operateEvent(event){
     mainScreen.textContent = "";
     console.log(`number array is ${numberArray}`);
     console.log(`operator array is ${operatorArray}`);
+    if(numberArray.length > 1 && operatorArray.length > 0){
+        calculate();
+    }
+    else{
+        console.log("calculate did not fire");
+    }
 }
+
+equalsButton.addEventListener(`click`, equalsEvent);
 
 numButtons.forEach((number) => {
     number.addEventListener(`click`, addToMainScreen);
@@ -71,3 +102,7 @@ numButtons.forEach((number) => {
 operators.forEach((operator) => {
     operator.addEventListener(`click`, operateEvent);
 });
+
+
+// after sum we need to delete whatever is on the screen
+// we need to take this into consideration for operate event too.
