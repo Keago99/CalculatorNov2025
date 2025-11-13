c8ea462b8fce61c929b657c1572c73d92fbcbe07
Keago99 <keatabde@gmail.com>
2025-11-13 15:45:11 +0200

    Add logic for various functions
    Add JS logic for clearButtonHandler
    Add JS logic for clearEverythingButtonHandler
    Test both buttons, edge cases included

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const mainScreen = document.querySelector(".mainScreen");
 const subScreen = document.querySelector(".subScreen");
 
 const equalsButton = document.querySelector(".equalsButton");
-const clearButton = document.querySelector(".clearButton");
+const clearButton = document.querySelector(".clear");
 const clearEverythingButton = document.querySelector(".clearEverything");
 
 const numButtons = document.querySelectorAll(".numButton")
@@ -12,33 +12,32 @@ const operatorButtons = document.querySelectorAll(".operator");
 numberArray = [];
 operatorArray = [];
 historyArray = [];
-const resetScreen = false;
+let resetScreen = false;
 
 
-function clearMainScreen(){
-    //
-}
 
-function clearSubScreen(){
-    // clear subScreen div
-}
 
 function updateMainScreen(content){
     if(mainScreen.textContent.trim().length <= 6){
         mainScreen.textContent += content;
     }
     else{
-        console.log("not firing");
+        console.log("too many numbers on screen");
     }
 
 }
 
-function clear(){
-    // clear button
+function clearButtonHandler(){
+    mainScreen.textContent = "";
+    resetScreen = false;
 }
 
-function clearEverything(){
-    // clear everything button
+function clearEverythingButtonHandler(){
+    numberArray.length = 0;
+    operatorArray.length = 0;
+    historyArray.length = 0;
+    mainScreen.textContent = "";
+    resetScreen = false;
 }
 
 function numButtonHandler(e){
@@ -61,3 +60,10 @@ function calculate(){
 numButtons.forEach((button) => {
     button.addEventListener(`click`, numButtonHandler);
 });
+
+operatorButtons.forEach((button) => {
+    button.addEventListener(`click`, operatorButtonHandler);
+});
+
+clearButton.addEventListener(`click`, clearButtonHandler);
+clearEverythingButton.addEventListener(`click`, clearEverythingButtonHandler);
